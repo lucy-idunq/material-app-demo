f28e93cf33dd28ddc51e1e96052e500c45d762fa
lucylyida <lucylyida188@gmail.com>
2020-12-26 17:15:42 +0630

    update

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,23 @@
 
-import { makeStyles } from '@material-ui/core';
+import { CssBaseline, makeStyles } from '@material-ui/core';
 import Header from './Components/Header';
 import SideMenu from './Components/SideMenu';
+
 const useStyles = makeStyles({
-  appMain:{
-    paddingLeft:'320px',
-    widht:'100%'
+  appMain: {
+    paddingLeft: '320px',
+    widht: '100%'
   }
 })
 function App() {
   const classes = useStyles();
   return (
     <>
-      <SideMenu/>
-    <div className={classes.appMain}> <Header/> </div> 
+      <SideMenu />
+      <div className={classes.appMain}>
+        <Header />
+      </div>
+    <CssBaseline/>
     </>
   );
 }
--- src/App/Components/Header.js
+++ src/App/Components/Header.js
@@ -1,15 +1,64 @@
 import React from 'react'
-import { AppBar, Grid, Toolbar } from '@material-ui/core';
+import { AppBar, Badge, Grid, IconButton, InputBase, makeStyles, Toolbar } from '@material-ui/core';
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
+import SearchIcon from '@material-ui/icons/Search';
+
+const useStyles =makeStyles({
+    root:{
+        backgroundColor:'#fff',
+    },
+    searchInput:{
+        opacity:'0.6',
+        padding:'0px 8px',
+        fontSize:'0.8rem',
+        '&:hover':{
+            backgroundColor:'#f2f2f2f2'
+        },
+        '& .MuiSvgIcon-root':{
+            marginRight:'8px'
+        },
+        btnRoot:{
+            backgroundColor:'green'
+        },
+        btnLabel:{
+            backgroundColor:'red'
+        }
+    }
+})
+
 const Header = props => {
+    const classes = useStyles();
+
     return (
-        <AppBar position='static'>
+        <AppBar position='static' className={classes.root}>
             <Toolbar>
-                <Grid container>
-                    <Grid item sm={4} style={{border:'1px solid #fff'}}>
-
+                <Grid container alignContent>
+                    <Grid item>
+                        <InputBase 
+                        placeholder="search"
+                        startAdornment={<SearchIcon  fontSize='small' />}
+                        className={classes.searchInput}
+                        />
                     </Grid>
-                    <Grid item sm={8} style={{border:'1px solid #000'}}>
-
+                    <Grid item sm></Grid>
+                    <Grid item>
+                        <IconButton classes={{root:classes.btnRoot,label:classes.btnLabel}}>
+                            <Badge badgeContent={4} color='secondary'>
+                                <NotificationsNoneIcon />
+                            </Badge>
+                        </IconButton>
+                        <IconButton >
+                            <Badge badgeContent={3} color='secondary'>
+                                <ChatBubbleOutlineIcon />
+                            </Badge>
+                        </IconButton>
+                        <IconButton>
+                            <Badge>
+                                <PowerSettingsNewIcon />
+                            </Badge>
+                        </IconButton>
                     </Grid>
                 </Grid>
             </Toolbar>
